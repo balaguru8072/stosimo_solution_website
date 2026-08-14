f540fc0298a74f7b3c0251db01162ca4572dd390
balaguru <balagurus8072@gmail.com>
2026-08-14 13:48:14 +0530

    router issues

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import HeaderNav from "./components/Header/HeaderNav"
 import FooterSection from "./components/Footer/FooterSection"
+import ScrollToTop from "./components/ScrollToTop"
 
 const Layout = () =>{
     return(
         <>
+            <ScrollToTop />
             <HeaderNav />
             <main>
                 <Outlet />
